4078106a8ddb24ff141113a2c29dbb858ae90f5b
takke <takke30@gmail.com>
2020-07-21 12:55:35 +0900

    parse -> secondary constructor

--- twitter4j-v2-support/src/main/kotlin/twitter4j/ImageUrl2.kt
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/ImageUrl2.kt
@@ -2,13 +2,9 @@ package twitter4j
 
 data class ImageUrl2(val url: String, val width: Int, val height: Int) {
 
-    companion object {
-        fun parse(json: JSONObject): ImageUrl2 {
-            return ImageUrl2(
-                    json.getString("url"),
-                    json.getInt("width"),
-                    json.getInt("height")
-            )
-        }
-    }
+    constructor(json: JSONObject) : this(
+            json.getString("url"),
+            json.getInt("width"),
+            json.getInt("height")
+    )
 }
--- twitter4j-v2-support/src/main/kotlin/twitter4j/UrlEntity2.kt
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/UrlEntity2.kt
@@ -12,28 +12,23 @@ data class UrlEntity2(
         val description: String,
         val unwoundUrl: String
 ) {
-    companion object {
-        fun parse(json: JSONObject): UrlEntity2 {
-
-            val images: MutableList<ImageUrl2> = mutableListOf()
-            json.getJSONArray("images").let { imagesArray ->
-                for (i in 0 until imagesArray.length()) {
-                    images.add(ImageUrl2.parse(imagesArray.getJSONObject(i)))
+    constructor(json: JSONObject) : this(
+            json.getString("url"),
+            json.getInt("start"),
+            json.getInt("end"),
+            json.getString("expanded_url"),
+            json.getString("display_url"),
+            mutableListOf<ImageUrl2>().also { images ->
+                json.getJSONArray("images").let { imagesArray ->
+                    for (i in 0 until imagesArray.length()) {
+                        images.add(ImageUrl2(imagesArray.getJSONObject(i)))
+                    }
                 }
-            }
+            },
+            json.getInt("status"),
+            json.getString("title"),
+            json.getString("description"),
+            json.getString("unwound_url")
+    )
 
-            return UrlEntity2(
-                    json.getString("url"),
-                    json.getInt("start"),
-                    json.getInt("end"),
-                    json.getString("expanded_url"),
-                    json.getString("display_url"),
-                    images,
-                    json.getInt("status"),
-                    json.getString("title"),
-                    json.getString("description"),
-                    json.getString("unwound_url")
-            )
-        }
-    }
 }
