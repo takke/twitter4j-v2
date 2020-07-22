3319d4730b3253546d69033a65f6a93b5fccd2e2
takke <takke30@gmail.com>
2020-07-22 14:39:59 +0900

    add UsersResponseTest

---
+++ twitter4j-v2-support/src/test/kotlin/twitter4j/UsersResponseTest.kt
@@ -0,0 +1,25 @@
+package twitter4j
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class UsersResponseTest {
+
+    @Test
+    fun minimumTweet() {
+
+        val res = UsersResponse(JSONObject("{\n" +
+                "   \"data\": [\n" +
+                "      {\n" +
+                "         \"id\": \"87532773\",\n" +
+                "         \"username\": \"TwitterDesign\",\n" +
+                "         \"name\": \"Twitter Design\"\n" +
+                "      }\n" +
+                "   ]\n" +
+                "}"))
+        assertThat(res.users.size).isEqualTo(1)
+        assertThat(res.users[0].id).isEqualTo(87532773)
+        assertThat(res.users[0].username).isEqualTo("TwitterDesign")
+        assertThat(res.users[0].name).isEqualTo("Twitter Design")
+    }
+}
