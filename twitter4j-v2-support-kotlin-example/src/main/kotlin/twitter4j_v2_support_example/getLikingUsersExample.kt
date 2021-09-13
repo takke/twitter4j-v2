fd7ec553c1f6602bef942139a0cdc828e10afe6d
takke <takke30@gmail.com>
2021-09-13 12:45:47 +0900

    add getLikingUsers

---
+++ twitter4j-v2-support-kotlin-example/src/main/kotlin/twitter4j_v2_support_example/getLikingUsersExample.kt
@@ -0,0 +1,47 @@
+package twitter4j_v2_support_example
+
+import twitter4j.*
+import twitter4j.conf.ConfigurationBuilder
+
+
+fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
+
+    //--------------------------------------------------
+    // prepare twitter instance
+    //--------------------------------------------------
+    val conf = ConfigurationBuilder()
+        .setJSONStoreEnabled(true)
+        .build()
+
+    val twitter = TwitterFactory(conf).instance
+//    val twitter = TwitterFactory.getSingleton()
+
+
+    //--------------------------------------------------
+    // getLikingUsers example
+    //--------------------------------------------------
+    val statusId = 1435645603065778176L
+    twitter.getLikingUsers(
+        statusId,
+        expansions = "pinned_tweet_id",
+        tweetFields = "attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,public_metrics,possibly_sensitive,referenced_tweets,reply_settings,source,text,withheld",
+        userFields = "created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld"
+    ).let {
+        println(it)
+
+        val json = JSONObject(TwitterObjectFactory.getRawJSON(it))
+        println(json.toString(3))
+    }
+
+    println("minimum query")
+    println("=============")
+    twitter.getLikingUsers(
+        statusId
+    ).let {
+        println(it)
+
+        val json = JSONObject(TwitterObjectFactory.getRawJSON(it))
+        println(json.toString(3))
+    }
+
+}
---
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/getLikingUsers.kt
@@ -0,0 +1,44 @@
+package twitter4j
+
+/**
+ * Allows you to get information about a Tweet’s liking users.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/tweets/likes/api-reference/get-tweets-id-liking_users"
+ */
+@Throws(TwitterException::class)
+fun Twitter.getLikingUsers(
+    tweetId: Long,
+    expansions: String? = null,
+    tweetFields: String? = null,
+    userFields: String? = null,
+): UsersResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    val params = ArrayList<HttpParameter>()
+
+    if (expansions != null) {
+        params.add(HttpParameter("expansions", expansions))
+    }
+
+    if (tweetFields != null) {
+        params.add(HttpParameter("tweet.fields", tweetFields))
+    }
+
+    if (userFields != null) {
+        params.add(HttpParameter("user.fields", userFields))
+    }
+
+    return UsersFactory().createUsersResponse(
+        http.get(
+            conf.v2Configuration.baseURL + "tweets/" + tweetId + "/liking_users",
+            params.toTypedArray(),
+            auth,
+            this
+        ),
+        conf
+    )
+}
