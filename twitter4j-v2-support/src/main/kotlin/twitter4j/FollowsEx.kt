74b51a91727d70a5f13dab147361f9f644efd8d8
takke <takke30@gmail.com>
2021-09-14 13:39:16 +0900

    merge to FollowsEx.kt

---
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/FollowsEx.kt
@@ -0,0 +1,155 @@
+package twitter4j
+
+/**
+ * Returns a list of users the specified user ID is following.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-following"
+ */
+@Throws(TwitterException::class)
+fun Twitter.getFollowingUsers(
+    userId: Long,
+    expansions: String? = "pinned_tweet_id",
+    maxResults: Int? = null,
+    paginationToken: String? = null,
+    tweetFields: String? = null,
+    userFields: String? = null,
+): UsersResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    val params = ArrayList<HttpParameter>()
+
+    if (expansions != null) {
+        params.add(HttpParameter("expansions", expansions))
+    }
+
+    if (maxResults != null) {
+        params.add(HttpParameter("max_results", maxResults))
+    }
+
+    if (paginationToken != null) {
+        params.add(HttpParameter("pagination_token", paginationToken))
+    }
+
+    if (tweetFields != null) {
+        params.add(HttpParameter("tweet.fields", tweetFields))
+    }
+
+    if (userFields != null) {
+        params.add(HttpParameter("user.fields", userFields))
+    }
+
+    return V2ResponseFactory().createUsersResponse(
+        http.get(conf.v2Configuration.baseURL + "users/" + userId + "/following", params.toTypedArray(), auth, this),
+        conf
+    )
+}
+
+/**
+ * Returns a list of users who are followers of the specified user ID.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-followers"
+ */
+@Throws(TwitterException::class)
+fun Twitter.getFollowerUsers(
+    userId: Long,
+    expansions: String? = "pinned_tweet_id",
+    maxResults: Int? = null,
+    paginationToken: String? = null,
+    tweetFields: String? = null,
+    userFields: String? = null
+): UsersResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    val params = ArrayList<HttpParameter>()
+
+    if (expansions != null) {
+        params.add(HttpParameter("expansions", expansions))
+    }
+
+    if (maxResults != null) {
+        params.add(HttpParameter("max_results", maxResults))
+    }
+
+    if (paginationToken != null) {
+        params.add(HttpParameter("pagination_token", paginationToken))
+    }
+
+    if (tweetFields != null) {
+        params.add(HttpParameter("tweet.fields", tweetFields))
+    }
+
+    if (userFields != null) {
+        params.add(HttpParameter("user.fields", userFields))
+    }
+
+    return V2ResponseFactory().createUsersResponse(
+        http.get(conf.v2Configuration.baseURL + "users/" + userId + "/followers", params.toTypedArray(), auth, this),
+        conf
+    )
+}
+
+/**
+ * Allows a user ID to follow another user.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/post-users-source_user_id-following"
+ */
+@Throws(TwitterException::class)
+fun Twitter.followUser(
+    sourceUserId: Long,
+    targetUserId: Long
+): FollowResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    val json = JSONObject()
+    json.put("target_user_id", targetUserId.toString())
+
+    return V2ResponseFactory().createFollowResponse(
+        http.post(
+            conf.v2Configuration.baseURL + "users/" + sourceUserId + "/following",
+            arrayOf(HttpParameter(json)),
+            auth,
+            this
+        ),
+        conf
+    )
+}
+
+/**
+ * Allows a user ID to unfollow another user.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/delete-users-source_id-following"
+ */
+@Throws(TwitterException::class)
+fun Twitter.unfollowUser(
+    sourceUserId: Long,
+    targetUserId: Long
+): BooleanResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    return V2ResponseFactory().createBooleanResponse(
+        http.delete(
+            conf.v2Configuration.baseURL + "users/" + sourceUserId + "/following/" + targetUserId,
+            emptyArray(),
+            auth,
+            this
+        ),
+        conf,
+        "following"
+    )
+}
