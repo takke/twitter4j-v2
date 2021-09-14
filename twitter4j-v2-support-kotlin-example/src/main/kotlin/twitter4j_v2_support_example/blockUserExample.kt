57c8ed539f4c78daf7b630d551a1bcf88124af26
takke <takke30@gmail.com>
2021-09-14 13:49:13 +0900

    add blockUser, unblockUser

---
+++ twitter4j-v2-support-kotlin-example/src/main/kotlin/twitter4j_v2_support_example/blockUserExample.kt
@@ -0,0 +1,59 @@
+package twitter4j_v2_support_example
+
+import twitter4j.*
+import twitter4j.conf.ConfigurationBuilder
+
+
+fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
+
+    //--------------------------------------------------
+    // prepare twitter instance
+    //--------------------------------------------------
+    val conf = ConfigurationBuilder()
+        .setJSONStoreEnabled(true)
+        .build()
+
+    val twitter = TwitterFactory(conf).instance
+//    val twitter = TwitterFactory.getSingleton()
+
+    // get my id
+    val myUser = twitter.verifyCredentials()
+    val myId = myUser.id
+
+
+    //--------------------------------------------------
+    // blockUser example
+    //--------------------------------------------------
+    val targetUserId = 14276577L // sample2
+    println("blockUser")
+    println("=========")
+    twitter.blockUser(
+        myId,
+        targetUserId
+    ).let {
+        println("result: " + it.result)
+        println(it)
+
+        val json = JSONObject(TwitterObjectFactory.getRawJSON(it))
+        println(json.toString(3))
+    }
+
+    println("waiting...")
+    Thread.sleep(3000)
+
+    //--------------------------------------------------
+    // unblockUser example
+    //--------------------------------------------------
+    println("unblockUser")
+    println("===========")
+    twitter.unblockUser(
+        myId,
+        targetUserId
+    ).let {
+        println("result: " + it.result)
+        println(it)
+
+        val json = JSONObject(TwitterObjectFactory.getRawJSON(it))
+        println(json.toString(3))
+    }
+}
--- twitter4j-v2-support/src/main/kotlin/twitter4j/BlocksEx.kt
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/BlocksEx.kt
@@ -47,3 +47,62 @@ fun Twitter.getBlockingUsers(
         conf
     )
 }
+
+/**
+ * Causes the user (in the path) to block the target user. The user (in the path) must match the user context authorizing the request.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/users/blocks/api-reference/post-users-user_id-blocking"
+ */
+@Throws(TwitterException::class)
+fun Twitter.blockUser(
+    sourceUserId: Long,
+    targetUserId: Long
+): BooleanResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    val json = JSONObject()
+    json.put("target_user_id", targetUserId.toString())
+
+    return V2ResponseFactory().createBooleanResponse(
+        http.post(
+            conf.v2Configuration.baseURL + "users/" + sourceUserId + "/blocking",
+            arrayOf(HttpParameter(json)),
+            auth,
+            this
+        ),
+        conf,
+        "blocking"
+    )
+}
+
+/**
+ * Allows a user or authenticated user ID to unblock another user.
+ *
+ * @throws TwitterException when Twitter service or network is unavailable
+ * @see "https://developer.twitter.com/en/docs/twitter-api/users/blocks/api-reference/delete-users-user_id-blocking"
+ */
+@Throws(TwitterException::class)
+fun Twitter.unblockUser(
+    sourceUserId: Long,
+    targetUserId: Long
+): BooleanResponse {
+
+    if (this !is TwitterImpl) throw IllegalStateException("invalid twitter4j impl")
+
+    ensureAuthorizationEnabled()
+
+    return V2ResponseFactory().createBooleanResponse(
+        http.delete(
+            conf.v2Configuration.baseURL + "users/" + sourceUserId + "/blocking/" + targetUserId,
+            emptyArray(),
+            auth,
+            this
+        ),
+        conf,
+        "blocking"
+    )
+}
