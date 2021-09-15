e8431982c29cc524b05d1ce1d9ca06c6d7f0bcef
takke <takke30@gmail.com>
2021-09-15 10:37:03 +0900

    add FollowsTest

---
+++ twitter4j-v2-support/src/test/kotlin/twitter4j/FollowsTest.kt
@@ -0,0 +1,65 @@
+package twitter4j
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class FollowsTest {
+
+    private val twitter by lazy { V2TestUtil.createTwitterInstance() }
+    private val myId by lazy { twitter.verifyCredentials().id }
+
+    @Test
+    fun getFollowingUsers_simple() {
+
+        val res = twitter.getFollowingUsers(myId, expansions = null)
+        println(res.users.size)
+
+        assertThat(res.meta?.resultCount).isGreaterThan(0)
+        assertThat(res.users[0].id).isGreaterThan(0)
+        assertThat(res.users[0].name.length).isGreaterThan(0)
+        assertThat(res.users[0].username.length).isGreaterThan(0)
+    }
+
+    @Test
+    fun getFollowerUsers_simple() {
+
+        val res = twitter.getFollowerUsers(myId, expansions = null)
+        println(res.users.size)
+
+        assertThat(res.meta?.resultCount).isGreaterThan(0)
+        assertThat(res.users[0].id).isGreaterThan(0)
+        assertThat(res.users[0].name.length).isGreaterThan(0)
+        assertThat(res.users[0].username.length).isGreaterThan(0)
+    }
+
+    @Test
+    fun follow_then_unfollow() {
+
+        val targetUserId = 14276577L // sample2
+
+        println("followUser")
+        println("==========")
+        twitter.followUser(myId, targetUserId).let { res ->
+            println(res)
+            val json = JSONObject(TwitterObjectFactory.getRawJSON(res))
+            println(json.toString(3))
+
+            assertThat(res.result.following).isEqualTo(true)
+            assertThat(res.result.pendingFollow).isEqualTo(false)
+        }
+
+        // delay
+        Thread.sleep(2000)
+
+        println("unfollowUser")
+        println("============")
+        twitter.unfollowUser(myId, targetUserId).let { res ->
+            println(res)
+            val json = JSONObject(TwitterObjectFactory.getRawJSON(res))
+            println(json.toString(3))
+
+            assertThat(res.result).isEqualTo(false)
+        }
+    }
+
+}
