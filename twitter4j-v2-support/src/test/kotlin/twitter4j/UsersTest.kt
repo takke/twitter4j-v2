28bffb4d6832d036f26625900c3b2a555146cd67
takke <takke30@gmail.com>
2022-07-05 08:20:26 +0900

    add UsersTest

---
+++ twitter4j-v2-support/src/test/kotlin/twitter4j/UsersTest.kt
@@ -0,0 +1,30 @@
+package twitter4j
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class UsersTest {
+
+    private val twitter by lazy { V2TestUtil.createTwitterInstance() }
+
+    @Test
+    fun usersBy() {
+
+        val res = twitter.getUsersBy(
+            "twitterdev",
+            tweetFields = V2DefaultFields.tweetFields,
+            userFields = V2DefaultFields.userFields,
+            expansions = "pinned_tweet_id"
+        )
+
+        println(res)
+
+        val json = JSONObject(TwitterObjectFactory.getRawJSON(res))
+        println(json.toString(3))
+
+        val user = res.users[0]
+        assertThat(user.id).isEqualTo(2244994945L)
+        assertThat(user.username).isEqualTo("TwitterDev")
+    }
+
+}
