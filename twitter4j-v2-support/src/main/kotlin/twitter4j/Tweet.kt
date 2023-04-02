90ebe30e1da3367c7e0eddadc41ed2640341d429
takke <takke30@gmail.com>
2023-04-02 23:23:57 +0900

    add isRetweet

--- twitter4j-v2-support/src/main/kotlin/twitter4j/Tweet.kt
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/Tweet.kt
@@ -95,6 +95,9 @@ data class Tweet(
         }
     }
 
+    val isRetweet: Boolean
+        get() = retweetId != null
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
