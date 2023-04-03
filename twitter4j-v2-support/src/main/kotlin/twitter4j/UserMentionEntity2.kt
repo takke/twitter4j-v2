66f73d757274f48d1458972e1f0ecec245bc3c09
takke <takke30@gmail.com>
2023-04-03 13:10:25 +0900

    UserMentionEntity2: TweetEntity

--- twitter4j-v2-support/src/main/kotlin/twitter4j/UserMentionEntity2.kt
+++ twitter4j-v2-support/src/main/kotlin/twitter4j/UserMentionEntity2.kt
@@ -1,11 +1,11 @@
 package twitter4j
 
 data class UserMentionEntity2(
-    val start: Int,
-    val end: Int,
+    val start_: Int,
+    val end_: Int,
     val screenName: String,
     val id: Long
-) {
+) : TweetEntity {
 
     constructor(json: JSONObject) : this(
         json.getInt("start"),
@@ -13,4 +13,16 @@ data class UserMentionEntity2(
         json.getString("username"),
         json.optLong("id", -1L)
     )
+
+    override fun getText(): String {
+        return screenName
+    }
+
+    override fun getStart(): Int {
+        return start_
+    }
+
+    override fun getEnd(): Int {
+        return end_
+    }
 }
